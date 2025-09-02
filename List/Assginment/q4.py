a5bb7cf3ea62ba1f98f0c890b57f373a90e14e3f
Vortex-16 <vikash9c35@gmail.com>
2025-09-02 12:34:54 +0530

    b y e

--- List/Assginment/q4.py
+++ List/Assginment/q4.py
@@ -1,1 +1,9 @@
-#Createw a lit of sting write a python program which create another list from the frist taking the fisrt taking the first charcter from each word
+#Create a lit of string write a python program which create another list from the frist taking the fisrt taking the first charcter from each word
+list=[]
+print("Python create another List:")
+
+'''
+Now you are asked to take the first Char from each word.
+That's Yet !!
+NVM Tired 😊(Win + FullStop){Not ChatGpt}
+'''
--- List/Assginment/q5.py
+++ List/Assginment/q5.py
@@ -1,1 +1,9 @@
 # Write A PYTHON PROGRAM THAT PASSES A LIST TO A FUNCTION THAT SQARES EACH ELEMENT IN THE LIST
+list=[]
+print("Python pass the List to the function that squares:")
+sq=[x**2 for x in range(10)]
+print(sq)
+'''
+Now you are asked to Print the list values at diffrent stages to show the changes 2**2=4; 3**2=9; 4**2=16
+NVM Tired 😊(Win + FullStop){Not ChatGpt}
+'''
