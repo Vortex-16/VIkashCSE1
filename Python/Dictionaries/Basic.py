3bc4b13fe6fe71a62632a31a7ce8e0ffb4092759
Vortex-16 <vikash9c35@gmail.com>
2025-10-14 10:37:10 +0530

    What is Dictionary

---
+++ Python/Dictionaries/Basic.py
@@ -0,0 +1,11 @@
+dict = {'Name': 'Vikash', 'Age': 19, 'Dept': 'CSE','Year': 1}
+print ("dict['Name']: ", dict['Name'])
+print ("dict['Age']: ", dict['Age'])
+print ("dict['Dept']: ", dict['Dept'])
+# print ("dict['Year']: ", dict['Year'])
+
+dict['Year']=2
+print ("dict['Year']: ", dict['Year'])
+dict['College']='STCET'
+print ("dict['College']: ", dict['College'])
+
