acbb8574afb5730e79d10c5f66a1f8ac2d624971
Vortex-16 <vikash9c35@gmail.com>
2025-10-14 10:13:02 +0530

    Q4

--- Python/Tuples/Class/Q8.py
+++ Python/Tuples/Class/Q8.py
@@ -1,3 +1,6 @@
+# write a python program to find the index of an item in a tuple. Convert the string to a tuple
+# and find the index of a character in the tuple. Cheeck for a item which is not present in the tuple.
+
 tuple=tuple("index tuple")
 print(tuple)
 index=tuple.index("p")
