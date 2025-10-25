c6eeafcb7e185afababc4e80e43e498e0ab0ce1c
Vikash Gupta <vikash9c35@gmail.com>
2025-10-25 13:20:30 +0000

    Done

--- Python/Dictionaries/Assignment/sumOfAllItem.py
+++ Python/Dictionaries/Assignment/sumOfAllItem.py
@@ -1,6 +1,9 @@
-# write a programk to sum all the items in a dictionary
+# write a program to sum all the items in a dictionary
+
 d={'a':100,'b':200,'c':300}
 sum=0
 for i in d.values():
     sum=sum+i
 print("The sum of all items in the dictionary is:",sum)
+
+# Output: The sum of all items in the dictionary is: 600
