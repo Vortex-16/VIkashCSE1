7f98198d624cff6f9e769176c63458ab3388a236
Vikash Gupta <vikash9c35@gmail.com>
2025-10-28 08:01:23 +0000

    fix

--- DSA/sorts.c
+++ DSA/sorts.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+// ---------------- Selection Sort ----------------
 void selectionSort(int arr[], int n) {
     for (int i = 0; i < n - 1; i++) {
         int min_idx = i;
@@ -13,6 +14,7 @@ void selectionSort(int arr[], int n) {
     }
 }
 
+// ---------------- Bubble Sort ----------------
 void bubbleSort(int arr[], int n) {
     for (int i = 0; i < n - 1; i++)
         for (int j = 0; j < n - i - 1; j++)
@@ -23,6 +25,7 @@ void bubbleSort(int arr[], int n) {
             }
 }
 
+// ---------------- Insertion Sort ----------------
 void insertionSort(int arr[], int n) {
     for (int i = 1; i < n; i++) {
         int key = arr[i];
@@ -35,37 +38,44 @@ void insertionSort(int arr[], int n) {
     }
 }
 
-int partition(int arr[], int low, int high) {
-    int pivot = arr[high];
-    int i = low - 1;
-    for (int j = low; j < high; j++) {
-        if (arr[j] < pivot) {
-            i++;
-            int temp = arr[i];
-            arr[i] = arr[j];
-            arr[j] = temp;
-        }
-    }
-    int temp = arr[i + 1];
-    arr[i + 1] = arr[high];
-    arr[high] = temp;
-    return i + 1;
+// ---------------- Merge Sort ----------------
+void merge(int arr[], int l, int m, int r) {
+    int n1 = m - l + 1, n2 = r - m;
+    int L[n1], R[n2];
+
+    for (int i = 0; i < n1; i++)
+        L[i] = arr[l + i];
+    for (int j = 0; j < n2; j++)
+        R[j] = arr[m + 1 + j];
+
+    int i = 0, j = 0, k = l;
+    while (i < n1 && j < n2)
+        arr[k++] = (L[i] <= R[j]) ? L[i++] : R[j++];
+
+    while (i < n1)
+        arr[k++] = L[i++];
+
+    while (j < n2)
+        arr[k++] = R[j++];
 }
 
-void quickSort(int arr[], int low, int high) {
-    if (low < high) {
-        int pi = partition(arr, low, high);
-        quickSort(arr, low, pi - 1);
-        quickSort(arr, pi + 1, high);
+void mergeSort(int arr[], int l, int r) {
+    if (l < r) {
+        int m = (l + r) / 2;
+        mergeSort(arr, l, m);
+        mergeSort(arr, m + 1, r);
+        merge(arr, l, m, r);
     }
 }
 
+// ---------------- Display ----------------
 void display(int arr[], int n) {
     for (int i = 0; i < n; i++)
         printf("%d ", arr[i]);
     printf("\n");
 }
 
+// ---------------- Main ----------------
 int main() {
     int n, choice;
     printf("Enter number of elements: ");
@@ -80,7 +90,7 @@ int main() {
         printf("1. Selection Sort\n");
         printf("2. Bubble Sort\n");
         printf("3. Insertion Sort\n");
-        printf("4. Quick Sort\n");
+        printf("4. Merge Sort\n");
         printf("5. Display Array\n");
         printf("6. Exit\n");
         printf("Enter your choice: ");
@@ -100,10 +110,11 @@ int main() {
                 printf("Array sorted using Insertion Sort.\n");
                 break;
             case 4:
-                quickSort(arr, 0, n - 1);
-                printf("Array sorted using Quick Sort.\n");
+                mergeSort(arr, 0, n - 1);
+                printf("Array sorted using Merge Sort.\n");
                 break;
             case 5:
+                printf("Current Array:\n");
                 display(arr, n);
                 break;
             case 6:
@@ -115,7 +126,9 @@ int main() {
     return 0;
 }
 
-/* Output: 
+/*
+Sample Output:
+----------------
 Enter number of elements: 6
 Enter elements:
 8 2 3 5 9 1
@@ -124,29 +137,13 @@ Menu:
 1. Selection Sort
 2. Bubble Sort
 3. Insertion Sort
-4. Quick Sort
+4. Merge Sort
 5. Display Array
 6. Exit
-Enter your choice: 3
-Array sorted using Insertion Sort.
+Enter your choice: 4
+Array sorted using Merge Sort.
 
 Menu:
-1. Selection Sort
-2. Bubble Sort
-3. Insertion Sort
-4. Quick Sort
-5. Display Array
-6. Exit
 Enter your choice: 5
-1 2 3 5 8 9 
-
-Menu:
-1. Selection Sort
-2. Bubble Sort
-3. Insertion Sort
-4. Quick Sort
-5. Display Array
-6. Exit
-Enter your choice: 6
-
+1 2 3 5 8 9
 */
