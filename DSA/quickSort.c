0ab9c2d51b67656873937d4e2f1d8461cbb90cb2
Vortex-16 <vikash9c35@gmail.com>
2025-11-21 14:17:11 +0530

    Fix 1

--- DSA/quickSort.c
+++ DSA/quickSort.c
@@ -6,6 +6,7 @@ void swap(int* a, int* b) {
     *a = *b;
     *b = temp;
 }
+
 int partition(int arr[], int low, int high) {
     int pivot = arr[high];
     int i = (low - 1);
@@ -18,6 +19,7 @@ int partition(int arr[], int low, int high) {
     swap(&arr[i + 1], &arr[high]);
     return (i + 1);
 }
+
 void quickSort(int arr[], int low, int high) {
     if (low < high) {
         int pi = partition(arr, low, high);
@@ -25,14 +27,24 @@ void quickSort(int arr[], int low, int high) {
         quickSort(arr, pi + 1, high);
     }
 }
+
 void printArray(int arr[], int size) {
     for (int i = 0; i < size; i++)
         printf("%d ", arr[i]);
     printf("\n");
 }
+
 int main() {
-    int arr[] = {10, 7, 8, 9, 1, 5};
-    int n = sizeof(arr) / sizeof(arr[0]);
+    int n;   
+    printf("Enter number of elements: ");
+    scanf("%d", &n);
+
+    int arr[n];
+    printf("Enter %d elements: ", n);
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
     quickSort(arr, 0, n - 1);
     printf("Sorted array: \n");
     printArray(arr, n);
