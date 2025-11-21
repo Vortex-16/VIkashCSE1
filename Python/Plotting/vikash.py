c44448f6d35df89ce5dc6f451f76c477a5648c17
Vikash Gupta <vikasharmy811@gmail.com>
2025-11-21 19:25:30 +0530

    Name Fix 1

--- Python/Plotting/vikash.py
+++ Python/Plotting/vikash.py
@@ -1,27 +1,38 @@
-'''
-Write your name using ploting library matplotlib in Python.
-Vikash 
-'''
 import matplotlib.pyplot as plt
-x=[1,2,3]
-y=[8,6,9]
-plt.plot(x,y,label='V')
-x=[1,2,3,5,1,6,7]
-y=[2,4,1,2,4,5,7]
-plt.plot(x,y,label='i')
-x=[1,2,3,4,5]
-y=[5,3,6,2,7]
-plt.plot(x,y,label='k')
-x=[1,2,3,4]
-y=[7,6,5,8]
-plt.plot(x,y,label='a')
-x=[1,2,3,4,5,6]
-y=[1,3,2,4,3,5]
-plt.plot(x,y,label='s')
-x=[1,2,3,4,5]
-y=[9,7,8,6,10]
-plt.plot(x,y,label='h')
-plt.xlabel('X-axis')
-plt.ylabel('Y-axis')
-plt.legend()
+import numpy as np
+
+plt.figure(figsize=(10, 6))
+
+# Letter V
+plt.plot([0, 1, 2], [5, 0, 5], label='V')
+
+# Letter I
+plt.plot([3, 3], [0, 5])
+plt.plot([2.5, 3.5], [5, 5])
+plt.plot([2.5, 3.5], [0, 0], label='I')
+
+# Letter K
+plt.plot([4, 4], [0, 5])                 # vertical line
+plt.plot([4, 6], [2.5, 5])               # upper diagonal
+plt.plot([4, 6], [2.5, 0], label='K')    # lower diagonal
+
+# Letter A
+plt.plot([7, 8.5, 10], [0, 5, 0])        # /\ shape
+plt.plot([7.7, 9.3], [2.5, 2.5], label='A')
+
+# Letter S
+t = np.linspace(0, 1, 100)
+sx = 12 + np.sin(2 * np.pi * t)
+sy = 3 + 2 * np.cos(np.pi * t)
+
+plt.plot(sx, sy, linewidth=2, color="cyan", label='S')
+
+# Letter H
+plt.plot([14, 14], [0, 5])               # left vertical
+plt.plot([16, 16], [0, 5])               # right vertical
+plt.plot([14, 16], [2.5, 2.5], label='H') # middle bar
+
+plt.title("VIKASH using Matplotlib")
+plt.gca().set_aspect("equal")
+plt.grid(True)
 plt.show()
