92f2af5ba97ee8fe778d2a3a57fd2b796b0f60db
Vortex-16 <vikash9c35@gmail.com>
2025-12-05 14:28:40 +0530

    dfs done

--- DSA/BFS/dfs.c
+++ DSA/BFS/dfs.c
@@ -1,11 +1,33 @@
+//Write a C program to implement Depth First Search (DFS) algorithm for graph traversal.
 #include <stdio.h>
-#include <stlib.h>
-
-int dfs(G){
-
+#include <stdlib.h>
+#define MAX 100
+int graph[MAX][MAX], visited[MAX], n;
+void dfs(int v) {
+    visited[v] = 1;
+    printf("%d ", v);
+    for (int i = 0; i < n; i++) {
+        if (graph[v][i] == 1 && !visited[i]) {
+            dfs(i);
+        }
+    }
 }
-
-
-void main(){
-
+int main() {
+    int edges, u, v;
+    printf("Enter number of vertices: ");
+    scanf("%d", &n);
+    printf("Enter number of edges: ");
+    scanf("%d", &edges);
+    for (int i = 0; i < edges; i++) {
+        printf("Enter edge (u v): ");
+        scanf("%d %d", &u, &v);
+        graph[u][v] = 1;
+        graph[v][u] = 1; 
+    }
+    for (int i = 0; i < n; i++) {
+        visited[i] = 0;
+    }
+    printf("DFS Traversal starting from vertex 0:\n");
+    dfs(0);
+    return 0;
 }
